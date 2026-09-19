095aa872008fb4b1c6c0c0abf6e86d2c3312bf4b
duanechan <2223483@slu.edu.ph>
2026-09-19 15:20:51 +0800

    fix(ci): add react/react-in-jsx-scope rule and set it to off

--- frontend/eslint.config.js
+++ frontend/eslint.config.js
@@ -23,6 +23,7 @@ export default defineConfig([
       },
     },
     rules: {
+      "react/react-in-jsx-scope": "off",
       "react/prop-types": "off",
       "react-hooks/rules-of-hooks": "error",
       "react-hooks/exhaustive-deps": "warn",
