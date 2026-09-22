c3d7e69834eaf4458b95f18dfa0c694a2e008aa1
duanechan <2223483@slu.edu.ph>
2026-09-22 18:26:38 +0800

    fix(backend): find and fix more conflict markers

--- backend/alembic/env.py
+++ backend/alembic/env.py
@@ -1,35 +1,15 @@
 from logging.config import fileConfig
 
-<<<<<<< HEAD
-from sqlalchemy import engine_from_config
-from sqlalchemy import pool
-
-from alembic import context
-import sys
-from pathlib import Path
-
-# Add backend directory to path so models can be found
-sys.path.append(str(Path(__file__).resolve().parents[1]))
-
-from app.database import Base
-from app.models import *  # noqa: F403
-from app.config import settings
-=======
 from sqlalchemy import engine_from_config, pool
 
 from alembic import context
 from app import models  # noqa: F401
 from app.config import settings
 from app.database import Base
->>>>>>> f50204938a048a9cc793b8f3109b81715c98ec32
 
 # this is the Alembic Config object, which provides
 # access to the values within the .ini file in use.
 config = context.config
-<<<<<<< HEAD
-=======
-
->>>>>>> f50204938a048a9cc793b8f3109b81715c98ec32
 config.set_main_option("sqlalchemy.url", settings.database_url)
 
 # Interpret the config file for Python logging.
@@ -87,14 +67,7 @@ def run_migrations_online() -> None:
     )
 
     with connectable.connect() as connection:
-<<<<<<< HEAD
-        context.configure(
-            connection=connection, target_metadata=target_metadata
-        )
-=======
         context.configure(connection=connection, target_metadata=target_metadata)
->>>>>>> f50204938a048a9cc793b8f3109b81715c98ec32
-
         with context.begin_transaction():
             context.run_migrations()
 
